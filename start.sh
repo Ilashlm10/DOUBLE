c0c34aaf301aa6f75b1279efb8f8fcd417e74512
Matrix TG <108185896+matrixbotz0@users.noreply.github.com>
2023-03-08 17:59:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /aEvaMaria
 fi
-cd /EvaMaria
+cd /aEvaMaria
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
