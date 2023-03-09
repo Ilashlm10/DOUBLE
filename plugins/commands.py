92e221e78c304995364aedb20a9f191a2f517ec6
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-03-09 19:23:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -53,8 +53,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(photo=random.choice(PICS))
-        await message.reply_text(
-             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+        await message.reply_text(script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
              reply_markup=reply_markup,
              parse_mode=enums.ParseMode.HTML
         )
@@ -84,8 +83,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(photo=random.choice(PICS))
-        await message.reply_text(
-             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+        await message.reply_text(script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
              reply_markup=reply_markup,
              parse_mode=enums.ParseMode.HTML
         )
