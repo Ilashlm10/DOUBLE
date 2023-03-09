5fbfab0aaa56152f5f1a1cc6a7eb227725b6703e
Matrix TG <108185896+matrixbotz0@users.noreply.github.com>
2023-03-09 13:51:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -54,7 +54,6 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo("start")
         await message.reply_text("START_TXT")
-            photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
