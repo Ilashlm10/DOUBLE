03d7e457f82c02a7dc26c5698d1a8e43735a04b7
Ilashlm10 <ilashadam3@gmail.com>
2024-07-25 20:26:44 +0400

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -373,12 +373,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
         try:
             if AUTH_CHANNEL and not await is_subscribed(client, query):
                 await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
+                return
                 await client.send_message
             chat_id=message.from_user.id,
             text="**THX FOR REQUEST**",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode=enums.ParseMode.MARKDOWN
-                return
     elif settings['botpm']:
                 await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
                 return
