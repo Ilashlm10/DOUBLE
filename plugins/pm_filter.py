44966e8b06ac2ff414f0649a6acc5eb8701af450
Ilashlm10 <ilashadam3@gmail.com>
2024-07-25 21:44:21 +0400

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -385,7 +385,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     protect_content=True if ident == "filep" else False 
                 )
                 await query.answer('Check PM, I have sent files in pm', show_alert=True)
-                return await client.send_message
+                return 
+                await client.send_message
             chat_id=message.from_user.id,
             text="**THX FOR REQUEST**",
             parse_mode=enums.ParseMode.MARKDOWN
