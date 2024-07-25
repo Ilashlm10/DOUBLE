b5bf88239b89ac3ed4046873c56e98d2bb249f81
Ilashlm10 <ilashadam3@gmail.com>
2024-07-25 14:13:44 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Greymattersbot/EvaMaria /EvaMaria
+  git clone https://github.com/Ilashlm10/DOUBLE /DOUBLE
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /DOUBLE
 fi
-cd /EvaMaria
+cd /DOUBLE
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
