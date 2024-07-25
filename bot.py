edc2945a0bcf09c4deff83317fe909ffa646e0b3
Ilashlm10 <ilashadam3@gmail.com>
2024-07-25 14:57:23 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,6 +17,8 @@ from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, PORT
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
+from datetime import date, datetime 
+import pytz
 
 class Bot(Client):
 
@@ -42,14 +44,17 @@ class Bot(Client):
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
         self.username = '@' + me.username
-        #web-response
+        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        logging.info(LOG_STR)
+        tz = pytz.timezone('Asia/Kolkata')
+        today = date.today()
+        now = datetime.now(tz)
+        time = now.strftime("%H:%M:%S %p")
+        await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
-
-        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-        logging.info(LOG_STR)
 
     async def stop(self, *args):
         await super().stop()
