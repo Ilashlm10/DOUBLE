f0c155ab61039f6b0396a5ec981161543bb8d94b
Ilashlm10 <ilashadam3@gmail.com>
2024-07-29 21:48:35 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,8 +11,8 @@ logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from database.ia_filterdb import Media
-from database.users_chats_db import Media2, Media3, Media4, Media5
+from database.ia_filterdb import Media2, Media3, Media4, Media5
+from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, PORT, LOG_CHANNEL 
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
