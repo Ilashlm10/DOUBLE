9110e5e819b3a2f7459dc1006271915dc63f32a8
Ilashlm10 <ilashadam3@gmail.com>
2024-07-30 20:09:38 +0400

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -206,8 +206,12 @@ async def send_for_index(bot, message):
                                      callback_data=f'index#accept4#{chat_id}#{last_msg_id}#{message.from_user.id}')
             ],
             [
-                InlineKeyboardButton('Index To both DB',
+                InlineKeyboardButton('Index To DB5',
                                      callback_data=f'index#accept5#{chat_id}#{last_msg_id}#{message.from_user.id}')
+            ],
+            [
+                InlineKeyboardButton('Index To DB6',
+                                     callback_data=f'index#accept6#{chat_id}#{last_msg_id}#{message.from_user.id}')
             ],
             [
                 InlineKeyboardButton('close', callback_data='close_data'),
@@ -243,8 +247,12 @@ async def send_for_index(bot, message):
                                  callback_data=f'index#accept4#{chat_id}#{last_msg_id}#{message.from_user.id}')
         ],
         [
-            InlineKeyboardButton('Index To both DB',
+            InlineKeyboardButton('Index To DB5',
                                  callback_data=f'index#accept5#{chat_id}#{last_msg_id}#{message.from_user.id}')
+        ],
+        [
+            InlineKeyboardButton('Index To DB6',
+                                     callback_data=f'index#accept6#{chat_id}#{last_msg_id}#{message.from_user.id}')
         ],
         [
             InlineKeyboardButton('Reject Index',
@@ -566,6 +574,83 @@ async def index_files_to_db4(lst_msg_id, chat, msg, bot):
                 tru = await check_file(media)
                 if tru == "okda":
                     aynav, vnay = await save_file5(media) 
+                    if aynav:
+                        total_files += 1
+                    elif vnay == 0:
+                        duplicate += 1
+                    elif vnay == 2:
+                        errors += 1
+                else:
+                    duplicate += 1                
+        except Exception as e:
+            logger.exception(e)
+            return await bot.send_message(msg.chat.id, f'<b>🚫 Error:</b> {e}\n\n<b>╭ ▸ ETC: </b>{remaining_time_str} ❙ Remaining:</b> <code>{remaining_index}</code>\n<b>├ ▸ Last Updated: <i>{ttime}</i></b>\n<b>╰ ▸ Time Taken: </b>{elapsed_time_str} <b>\n\n<b>╭ ▸ Fetched:</b> <code>{current}</code>\n<b>├ ▸ Saved:</b> <code>{total_files}</code>\n<b>├ ▸ Duplicate:</b> <code>{duplicate}</code>\n<b>╰ ▸ Non:</b> <code>{no_media}</code>\n')
+        else:
+            await bot.send_message(msg.chat.id, f'<b>✅ Successfully Completed!!</b>\n\n<b>╭ ▸ ETC: </b>{remaining_time_str} ❙ Remaining:</b> <code>{remaining_index}</code>\n<b>├ ▸ Last Updated: <i>{ttime}</i></b>\n<b>╰ ▸ Time Taken: </b>{elapsed_time_str} <b>\n\n<b>╭ ▸ Fetched:</b> <code>{current}</code>\n<b>├ ▸ Saved:</b> <code>{total_files}</code>\n<b>├ ▸ Duplicate:</b> <code>{duplicate}</code>\n<b>╰ ▸ Non:</b> <code>{no_media}</code>\n')
+
+async def index_files_to_db6(lst_msg_id, chat, msg, bot):
+    total_files = 0
+    duplicate = 0
+    no_media = 0
+    fst_msg_id = temp.CURRENT
+    start_time = time.time()
+    remaining_time_str = "N/A"
+    async with lock:
+        try:
+            current = temp.CURRENT
+            tz = pytz.timezone('Asia/Kolkata')
+            today = datetime.date.today()
+            now = datetime.datetime.now(tz)
+            ttime = now.strftime("%I:%M:%S %p - %d %b, %Y")
+            temp.CANCEL = False
+            elapsed_time = 0
+            remaining_index = 0
+            async for message in bot.iter_messages(chat, lst_msg_id, temp.CURRENT):
+                if temp.CANCEL:
+                    await msg.edit(f"<b>❌ Successfully Cancelled!!</b>\n\n<b>├ ▸ Last Updated: <i>{ttime}</i></b>\n\n<b>╭ ▸ Fetched:</b> <code>{current}</code>\n<b>├ ▸ Saved:</b> <code>{total_files}</code>\n<b>├ ▸ Duplicate:</b> <code>{duplicate}</code>\n<b>╰ ▸ Non:</b> <code>{no_media}</code>\n")
+                    break
+                current += 1
+                tz = pytz.timezone('Asia/Kolkata')
+                today = datetime.date.today()
+                now = datetime.datetime.now(tz)
+                ttime = now.strftime("%I:%M:%S %p - %d %b, %Y")
+                if current % 250 == 0:
+                    can = [[InlineKeyboardButton('Cancel', callback_data='index_cancel')]]
+                    reply = InlineKeyboardMarkup(can)
+                    await asyncio.sleep(1)
+                    elapsed_time = time.time() - start_time
+                    remaining_time = (lst_msg_id - current - 1) * elapsed_time / (current - fst_msg_id + 1)
+                    remaining_time_str = get_readable_time(remaining_time)
+                    elapsed_time_str = get_readable_time(elapsed_time)
+                    remaining_index = lst_msg_id - current
+                    await msg.edit_text(
+                        text=f"<b>╭ ▸ ETC: </b>{remaining_time_str} ❙ Remaining:</b> <code>{remaining_index}</code>\n<b>├ ▸ Last Updated: <i>{ttime}</i></b>\n<b>╰ ▸ Time Taken: </b>{elapsed_time_str} <b>\n\n<b>╭ ▸ Fetched:</b> <code>{current}</code>\n<b>├ ▸ Saved:</b> <code>{total_files}</code>\n<b>├ ▸ Duplicate:</b> <code>{duplicate}</code>\n<b>╰ ▸ Non:</b> <code>{no_media}</code>\n",
+                        reply_markup=reply)
+                if message.empty:
+                    no_media += 1
+                    continue
+                elif not message.media:
+                    no_media += 1
+                    continue
+                elif message.media not in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.DOCUMENT]:
+                    no_media += 1
+                    continue
+                media = getattr(message, message.media.value, None)
+                if not media:
+                    no_media += 1
+                    continue
+                if media.mime_type not in ['video/mp4', 'video/x-matroska']:
+                    no_media += 1
+                    continue
+                media.file_type = message.media.value
+                media.caption = message.caption
+                tz = pytz.timezone('Asia/Kolkata')
+                today = datetime.date.today()
+                now = datetime.datetime.now(tz)
+                ttime = now.strftime("%I:%M:%S %p - %d %b, %Y")
+                tru = await check_file(media)
+                if tru == "okda":
+                    aynav, vnay = await save_file6(media) 
                     if aynav:
                         total_files += 1
                     elif vnay == 0:
