c0938cda3d94b8dd51a98f42163f3d6dd270211c
Ilashlm10 <ilashadam3@gmail.com>
2024-07-30 11:17:14 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,18 +7,41 @@ logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
+import os 
+import sys
+from dotenv import load_dotenv
+
+load_dotenv("./dynamic.env", override=True, encoding="utf-8")
+
+from pyrogram import idle
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media2, Media3, Media4, Media5
 from database.users_chats_db import db
 from database.join_reqs import JoinReqs
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, REQ_CHANNEL, LOG_CHANNEL 
-from utils import temp
-from Script import script
 from datetime import date, datetime 
 import pytz
+from info import *
+from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
+
+from aiohttp import web
+from plugins import web_server
+from plugins.index import index_files_to_db, incol
+PORT = environ.get("PORT", "8080")
+name = "main"
+
+async def restart_index(bot):
+    progress_document = incol.find_one({"_id": "index_progress"})
+    if progress_document:
+        last_indexed_file = progress_document.get("last_indexed_file", 0)
+        last_msg_id = progress_document.get("last_msg_id")
+        chat_id = progress_document.get("chat_id")           
+        temp.CURRENT = int(last_indexed_file)
+        msg = await bot.send_message(chat_id=int(LOG_CHANNEL), text="Restarting Index...")
+        await index_files_to_db(last_msg_id, chat_id, msg, bot)                    
+
 
 class Bot(Client):
 
@@ -62,6 +85,13 @@ class Bot(Client):
         time = now.strftime("%H:%M:%S %p")
         await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
 
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()       
+
+        await restart_index(self)
+    
     async def stop(self, *args):
         await super().stop()
         logging.info("Bot stopped. Bye.")
@@ -105,6 +135,7 @@ class Bot(Client):
                 yield message
                 current += 1
 
-
-app = Bot()
-app.run()
+if name == "main":
+    app = Bot()
+    app.run()
+    
